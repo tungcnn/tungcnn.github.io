e5baf74a6298948b05b2c1dfe95dc1ada37e302d
tungcnn <30380247+tungcnn@users.noreply.github.com>
2019-09-23 09:48:38 +1000

    asdasd

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const port = process.env.PORT || 3000;
-const base = `${__dirname}`;
+const base = `${__dirname}/public`;
 
 app.use(express.static('public'));
 
