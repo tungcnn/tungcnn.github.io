3b578af5b61e3a35469ecce4f60a01dd4300bac4
tungcnn <30380247+tungcnn@users.noreply.github.com>
2019-09-23 08:15:59 +1000

    changed

--- Web/public/app.js
+++ Web/public/app.js
@@ -1,7 +1,7 @@
 $('#navbar').load('navbar.html');
 $('#footer').load('footer.html');
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://tungcnn.github.io/hannoying.github.io';
 
 $.get(`${API_URL}/homepage`)
    
