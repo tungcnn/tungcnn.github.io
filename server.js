e1a80d2d2ebdf5865e81c0a2376429afde33ad27
tungcnn <30380247+tungcnn@users.noreply.github.com>
2019-09-23 10:27:38 +1000

    asd

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const port = process.env.PORT || 3000;
 const base = `${__dirname}/public`;
-
+app.use(cors());
 app.use(express.static('public'));
 
 app.use((req, res, next) => {
